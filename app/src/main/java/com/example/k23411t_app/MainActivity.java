d1e9719cc2ddfa80db65678a2f36ec110f31d37c
khaidao <daok257@gmail.com>
2026-04-29 07:09:36 +0700

    init project

---
+++ app/src/main/java/com/example/k23411t_app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.k23411t_app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void say_hello(View view) {
+        Toast.makeText(this, "Hello K23411T", Toast.LENGTH_LONG).show();
+    }
+
+    public void close_app(View view) {
+        finish();
+    }
+
+    public void show_major(View view) {
+        String my_major = getString(R.string.str_major);
+        Toast.makeText(this, my_major, Toast.LENGTH_LONG).show();
+    }
+}
